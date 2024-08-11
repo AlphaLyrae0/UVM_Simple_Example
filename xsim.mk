5e9afda6a32f07e7bd158c2cdfe1dbe6f8403347
Orimitsu Serizawa <weave_light-00@yahoo.co.jp>
2024-08-11 16:51:34 +0900

    Updated xsim.mk

--- xsim.mk
+++ xsim.mk
@@ -4,35 +4,34 @@ include common.mk
 #VIVADO_VER  := 2022.2
  VIVADO_VER  := 2023.1
  VIVADO_HOME := /tools/Xilinx/Vivado/$(VIVADO_VER)
- VLOG := $(VIVADO_HOME)/bin/xvlog
- ELAB := $(VIVADO_HOME)/bin/xelab
- SIM  := $(VIVADO_HOME)/bin/xsim
+ XVLOG := $(VIVADO_HOME)/bin/xvlog
+ XELAB := $(VIVADO_HOME)/bin/xelab
+ XSIM  := $(VIVADO_HOME)/bin/xsim
 
  TARGET := ./xsim.dir/$(TOP_MODULE)/xsimk
-run : $(TARGET)
-	$(SIM) $(TOP_MODULE) -testplusarg UVM_TESTNAME=$(TEST_NAME) -R
+#run : $(TARGET)
+#	$(XSIM) $(TOP_MODULE) -testplusarg UVM_TESTNAME=$(TEST_NAME) -R
+run : ./all.wdb
+./all.wdb : $(TARGET) ./dump_wdb.tcl
+	$(XSIM) $(TOP_MODULE) -testplusarg UVM_TESTNAME=$(TEST_NAME) -tclbatch dump_wdb.tcl -wdb ./all.wdb
 
 gui : $(TARGET)
-	$(SIM) $(TOP_MODULE) -testplusarg UVM_TESTNAME=$(TEST_NAME) -gui &
+	$(XSIM) $(TOP_MODULE) -testplusarg UVM_TESTNAME=$(TEST_NAME) -gui &
 
 log :
 	code xsim.log
 
+wave : ./all.wdb
+	$(XSIM) all.wdb -gui &
+
 clean :
-	rm -fr xsim.dir axsim.sh .Xil *.pb *.log *.jou *.str *.vcd *.wdb
+	rm -fr xsim.dir axsim.sh .Xil *.pb *.log *.jou *.str *.vcd *.wdb dump_wdb.tcl
 
 help  :
-	$(VLOG) -help; $(ELAB) -help; $(XSIM) -help
+	$(XVLOG) -help; $(XELAB) -help; $(XSIM) -help
 
 build :
 	make -B $(TARGET)
-
-TARGET_ALONE := ./xsim.dir/$(TOP_MODULE).alone/axsim ./axsim.sh
-alone : $(TARGET_ALONE)
-	./axsim.sh -testplusarg UVM_TESTNAME=$(TEST_NAME)
-
-build_alone :
-	make -B $(TARGET_ALONE)
 
 #-------- For Internal UVM ----------
  VLOG_OPT := -L uvm
@@ -49,9 +48,24 @@ VLOG_OPT += --include ./Env
 VLOG_OPT += --include ./Seq
 VLOG_OPT += --include ./Test
 $(TARGET) : $(SRC_FILES) $(INC_FILES)
-	$(VLOG) -sv $(VLOG_OPT) $(SRC_FILES) 
-	$(ELAB) $(TOP_MODULE) $(ELAB_OPT) -timescale 1ns/1ps -snapshot $(TOP_MODULE) -debug typical
+	$(XVLOG) -sv $(VLOG_OPT) $(SRC_FILES) 
+	$(XELAB) $(TOP_MODULE) $(ELAB_OPT) -timescale 1ns/1ps -snapshot $(TOP_MODULE) -debug typical
+
+./dump_wdb.tcl :
+	echo 'log_wave -r *'  > $@
+	echo 'run all'       >> $@
+	echo 'quit'          >> $@
+
+
+
+#------------ For Alone Run --------------------
+TARGET_ALONE := ./xsim.dir/$(TOP_MODULE).alone/axsim ./axsim.sh
+run_alone : $(TARGET_ALONE)
+	./axsim.sh -testplusarg UVM_TESTNAME=$(TEST_NAME)
+
+build_alone :
+	make -B $(TARGET_ALONE)
 
 $(TARGET_ALONE) : $(SRC_FILES) $(INC_FILES)
-	$(VLOG) -sv $(VLOG_OPT) $(SRC_FILES) 
-	$(ELAB) $(TOP_MODULE) $(ELAB_OPT) -timescale 1ns/1ps -snapshot $(TOP_MODULE).alone -standalone
+	$(XVLOG) -sv $(VLOG_OPT) $(SRC_FILES) 
+	$(XELAB) $(TOP_MODULE) $(ELAB_OPT) -timescale 1ns/1ps -snapshot $(TOP_MODULE).alone -standalone
