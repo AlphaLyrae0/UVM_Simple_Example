0537a9db1636ff69fc78ddd754597d0dac1b678d
Orimitsu Serizawa <weave_light-00@yahoo.co.jp>
2025-03-01 15:11:30 +0900

    Added files.f

---
+++ files.f
@@ -0,0 +1,17 @@
+./DUT/memory.sv
+
++incdir+Agent
+./Agent/mem_agent_pkg.sv
+./Agent/mem_if.sv
+
++inccir+Env
+./Env/mem_env_pkg.sv
+
++incdir+Sequence
+./Seq/mem_sequence_lib_pkg.sv
+
++incdir+Test
+./Test/mem_test_lib_pkg.sv
+
++incdir+TB
+./TB/mem_testbench.sv
